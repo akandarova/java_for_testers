e037c8c13627cddefc4acb053ca6f43b7e5aefd9
alina1303 <7759682@gmail.com>
2025-02-10 21:22:26 +0500

    Добавлен модуль Sandbox

---
+++ sandbox/src/main/java/Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Привет! У меня была сессия, поэтому буду сдавать домашнюю работу с опозданием, но постараюсь в скором времени нагнать!");
+    }
+}
