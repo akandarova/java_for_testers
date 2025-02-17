1025e36d8dd5fc43d9cdbba6727c7b923c14c3a4
alina1303 <7759682@gmail.com>
2025-02-17 17:13:21 +0500

    Создано исключение в классе TriangleTests

--- sandbox/src/test/java/TriangleTests.java
+++ sandbox/src/test/java/TriangleTests.java
@@ -7,13 +7,19 @@ public class TriangleTests {
     void canCalculatePerimeter(){
         var t = new Triangle(4.0, 5.0, 3.0);
         var result = t.trianglePerimeter();
-        Assertions.assertEquals(12.0, result);
+//        Assertions.assertEquals(12.0, result);
+        if (result != 12.0) {
+            throw new AssertionError(String.format("Expected: %f, actual: %f", 6.0, result));
+        }
     }
     @Test
     void canCalculateArea(){
         var t = new Triangle(4.0, 5.0, 3.0);
         var result = t.triangleArea();
-        Assertions.assertEquals(6, result);
+//        Assertions.assertEquals(6.0, result);
+        if (result != 6.0) {
+            throw new AssertionError(String.format("Expected: %f, actual: %f", 6.0, result));
+        }
     }
 
 }
