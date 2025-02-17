5ba2792785122c35da6a4904ffe14ea11e72d76b
alina1303 <7759682@gmail.com>
2025-02-17 17:25:31 +0500

    Создано исключение для случая, если сторона треугольника отрицательна

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -2,22 +2,22 @@ package ru.stqa.geometry.figures;
 
 public record Triangle(double sideA, double sideB, double sideC) {
 
-
+    public Triangle{
+        if (sideA < 0 || sideB < 0 || sideC < 0){
+            throw new IllegalArgumentException("Triangle side should be non-negative");
+        }
+    }
 
     static void printTriangleArea(Triangle t) {
         String text = String.format("Площадь треугольника со сторонами %f, %f и %f = %f", t.sideA, t.sideB, t.sideC, t.triangleArea());
         System.out.println(text);
     }
 
-
-
     public static void printTrianglePerimeter(Triangle t){
         String text = String.format("Периметр треугольника со сторонами %f, %f и %f = %f", t.sideA, t.sideB, t.sideC, t.trianglePerimeter());
         System.out.println(text);
 
     }
-
-
 
     public double triangleArea() {
         return Math.sqrt(0.5 * trianglePerimeter() * (0.5 * trianglePerimeter() - this.sideA) * (0.5 * trianglePerimeter() - this.sideB) * (0.5 * trianglePerimeter() - this.sideC));
--- sandbox/src/test/java/TriangleTests.java
+++ sandbox/src/test/java/TriangleTests.java
@@ -7,20 +7,32 @@ public class TriangleTests {
     void canCalculatePerimeter(){
         var t = new Triangle(4.0, 5.0, 3.0);
         var result = t.trianglePerimeter();
-//        Assertions.assertEquals(12.0, result);
-        if (result != 12.0) {
-            throw new AssertionError(String.format("Expected: %f, actual: %f", 6.0, result));
-        }
+        Assertions.assertEquals(12.0, result);
+//        if (result != 12.0) {
+//            throw new AssertionError(String.format("Expected: %f, actual: %f", 6.0, result));
+//        }
     }
     @Test
     void canCalculateArea(){
         var t = new Triangle(4.0, 5.0, 3.0);
         var result = t.triangleArea();
-//        Assertions.assertEquals(6.0, result);
-        if (result != 6.0) {
-            throw new AssertionError(String.format("Expected: %f, actual: %f", 6.0, result));
+        Assertions.assertEquals(6.0, result);
+//        if (result != 6.0) {
+//            throw new AssertionError(String.format("Expected: %f, actual: %f", 6.0, result));
+//        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSide(){
+        try{
+            new Triangle(-2.0, 5.0, 3.0);
+            Assertions.fail();
+        }
+        catch (IllegalArgumentException exception){
+            //ok
         }
     }
+    
 
 }
 
