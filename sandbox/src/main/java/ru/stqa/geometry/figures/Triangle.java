15ed55babaccd4e93ac4f83ce87414894c97f14e
alina1303 <7759682@gmail.com>
2025-02-18 21:45:01 +0500

    Создан тест для сравнения

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,15 +1,19 @@
 package ru.stqa.geometry.figures;
 
+import java.awt.*;
+import java.util.Objects;
+
 public record Triangle(double sideA, double sideB, double sideC) {
 
-    public Triangle{
-        if (sideA < 0 || sideB < 0 || sideC < 0){
-            throw new IllegalArgumentException("Triangle side should be non-negative");
-        }
-        else if ((sideA + sideB) < sideC || (sideB + sideC) < sideA || (sideA + sideC) > sideB){
-            throw new IllegalArgumentException("The sum of two sides cannot be less than the third");
-        }
-    }
+//    public Triangle {
+//        if (sideA < 0 || sideB < 0 || sideC < 0) {
+//            throw new IllegalArgumentException("Triangle side should be non-negative");
+//        }
+//
+//        if ((sideA + sideB) < sideC || (sideB + sideC) < sideA || (sideA + sideC) > sideB){
+//            throw new IllegalArgumentException("The sum of two sides cannot be less than the third");
+//        }
+//    }
 
     static void printTriangleArea(Triangle t) {
         String text = String.format("Площадь треугольника со сторонами %f, %f и %f = %f", t.sideA, t.sideB, t.sideC, t.triangleArea());
@@ -28,6 +32,20 @@ public record Triangle(double sideA, double sideB, double sideC) {
 
     public double trianglePerimeter() {
         return this.sideA + this.sideB + this.sideC;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return (Double.compare(this.sideA, triangle.sideA) == 0 && Double.compare(this.sideB, triangle.sideB) == 0 && Double.compare(this.sideC, triangle.sideC) == 0)
+                || (Double.compare(this.sideA, triangle.sideB) == 0 && Double.compare(this.sideB, triangle.sideC) == 0 && Double.compare(this.sideC, triangle.sideA) == 0)
+        || (Double.compare(this.sideA, triangle.sideC) == 0 && Double.compare(this.sideB, triangle.sideA) == 0 && Double.compare(this.sideC, triangle.sideB) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sideA, sideB, sideC);
     }
 }
 
--- sandbox/src/test/java/TriangleTests.java
+++ sandbox/src/test/java/TriangleTests.java
@@ -42,5 +42,12 @@ public class TriangleTests {
         }
     }
 
+    @Test
+    void testEquality(){
+        var t1 = new Triangle(4.0, 5.0, 3.0);
+        var t2 = new Triangle(5.0, 3.0, 4.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
 }
 
